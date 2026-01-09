fce2e22d0c7ebe815d91be753db51e907e855492
Rildo Tola <tolawonda@gmail.com>
2026-01-09 23:25:52 +0100

    Building the future with code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 console.log("Portfolio loaded");  // test that JS is working
 console.log("JavaScript is connected");
 
+
+
+const nameHeading = document.querySelector("#name");
+
+nameHeading.textContent = "Rildo Tola - Front-End Developer";
+
+nameHeading.style.color = "#4f46e5";
+
 // Get elements from the page
 const button = document.getElementById("changeTextBtn");
-const message = document.getElementById("message");
 
 // Listen for a click
 button.addEventListener("click", () => {
-  message.textContent = "You clicked the button!";
+  nameHeading.textContent = "Building the future with code 🚀";
 });
