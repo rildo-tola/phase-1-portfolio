081cbaad1c9156d0cd2d9bd78aa34911714ee392
Rildo Tola <tolawonda@gmail.com>
2026-01-09 22:13:03 +0100

    Add basic JavaScript interaction

--- phase-1-portfolio/js/main.js
+++ phase-1-portfolio/js/main.js
@@ -1,1 +1,11 @@
 console.log("Portfolio loaded");  // test that JS is working
+console.log("JavaScript is connected");
+
+// Get elements from the page
+const button = document.getElementById("changeTextBtn");
+const message = document.getElementById("message");
+
+// Listen for a click
+button.addEventListener("click", () => {
+  message.textContent = "You clicked the button!";
+});
