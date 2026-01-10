1a59d8e73efd2d53b0ceaab8f87b4ffc12d909ae
Rildo Tola <tolawonda@gmail.com>
2026-01-11 00:36:59 +0100

    fixed button functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 console.log("Portfolio loaded");  // test that JS is working
 console.log("JavaScript is connected");
 
-
-
 const nameHeading = document.querySelector("#name");
 
 nameHeading.textContent = "Rildo Tola - Front-End Developer";
+
+const ogText = "Rildo Tola - Front-End Developer";
+
+const newText = "Building the future with code 🚀";
 
 nameHeading.style.color = "#4f46e5";
 
@@ -14,5 +16,9 @@ const button = document.getElementById("changeTextBtn");
 
 // Listen for a click
 button.addEventListener("click", () => {
-  nameHeading.textContent = "Building the future with code 🚀";
+  if (nameHeading.textContent === ogText){
+nameHeading.textContent = newText
+  } else{
+nameHeading.textContent = ogText
+  }
 });
